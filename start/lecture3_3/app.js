c53f6c384185ac26b8262bd83eaf92e018c1483b
Josiah Zacharias <51419063+jomandaman@users.noreply.github.com>
2023-02-14 01:56:54 -0800

    Current progress

--- start/lecture3_3/app.js
+++ start/lecture3_3/app.js
@@ -6,6 +6,23 @@ class App{
 		const container = document.createElement( 'div' );
 		document.body.appendChild( container );
 
+		this.camera = new THREE.PerspectiveCamera( 60, window.innerWidth/ 
+			window.innerHeight, 0.1, 100);
+		this.camera.position.set( 0, 0, 4);
+		
+		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color( 0xAAAAAA );
+
+		this.renderer = new THREE.WebGL1Renderer( { antialias: true });
+		this.renderer.setPixelRatio( window.devicePixelRatio );
+		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		container.appendChild( this.renderer.domElement );
+
+		// Needs to render scene repeatedly for updates, animation loop
+		// bind helps ensure scope is app instance
+		// now will be called 60x per sec
+		this.renderer.setAnimationLoop( this.render.bind(this) );
+
         window.addEventListener('resize', this.resize.bind(this) );
 	}	
     
@@ -14,7 +31,7 @@ class App{
     }
     
 	render( ) {   
-		
+		this.renderer.render( this.scene, this.camera);
     }
 }
 
